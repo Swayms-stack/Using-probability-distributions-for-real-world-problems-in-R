252a583bc9fbb418ad936e68bc5c035bd18b8a9b
Swayms-stack <61129837+Swayms-stack@users.noreply.github.com>
2021-04-24 12:13:51 +0530

    Add files via upload

---
+++ code_project.R
@@ -0,0 +1,83 @@
+# Course - Using probability distributions for real world problems in R
+
+library(ggplot2)
+
+#-------------------------------------------------------------------------------
+# Binomial distribution
+
+# Problem
+# Suppose there are ten multiple choice questions in an English class quiz. 
+# Each question has four possible answers, and only one of them is correct.
+# 
+# Q1. Find the probability of having exactly 4 correct answers by random 
+# Solution
+
+
+# Q2. What is the probability of having four or less correct answers if a 
+#     student attempts to answer every question at random?
+# Solution
+
+
+
+#-------------------------------------------------------------------------------
+# Poisson distribution
+
+# Problem
+# If there are ten cars crossing a bridge per minute on average, find the 
+# probability of having seventeen or more cars crossing the bridge in a 
+# particular minute.
+# Solution
+
+#-------------------------------------------------------------------------------
+# Normal distribution
+
+# Problem
+# The Iris dataset in R contains iris flower specifications. The column 
+# Sepal Width follows a normal distribution.
+#
+# Q1. What is the percentage of flowers with Sepal Width 3cm or less?
+# Solution
+
+
+# Q2. What fraction of the flowers where Sepal Width lies between 3cm and 4cm?
+# Solution
+
+
+
+#-------------------------------------------------------------------------------
+# Exponential distribution
+#
+# Q1. Suppose the mean arrival time of a customer at a supermarket cashier is 
+# four minutes. Find the probability of a customer arriving at the cashier 
+# in less than three minutes.
+# Solution
+
+
+# Q2. The number of days ahead travelers buy their flight tickets follows an 
+# exponential distribution with the average number of days = 15. 
+# Find the probability that a traveler will purchase a ticket more than 
+# 10 days in advance.
+# Solution
+
+
+#-------------------------------------------------------------------------------
+# Chi-Square distribution
+
+# Problem Statement
+# Say, on average, a battery lasts 50 minutes on a single charge. The standard 
+# deviation is 3 minutes. Suppose the battery manufacturer runs a quality 
+# control test. They randomly select 9 batteries. The standard deviation of 
+# the selected batteries is 5 minutes. 
+# Q1. What would be the chi-square statistic represented by this test?
+# Solution
+
+
+# Q2. Suppose they repeated the test with a new random sample of 9 batteries. 
+# What is the probability that the standard deviation in the new test would be 
+# greater than 5 minutes?
+# Solution
+
+
+#-------------------------------------------------------------------------------
+### End of course.
+### Thank you for pursuing it.
